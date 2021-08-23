757ce35de3aa6f331c4a498519446d9796dd629b
fishkitten <_bumble-bee@mail.ru>
2021-08-23 20:36:11 +0300

    task 2

--- HomeWorkApp2.java
+++ HomeWorkApp2.java
@@ -1,5 +1,9 @@
 public class HomeWorkApp2 {
 	public static void main(String[] args) {
+		boolean check = checkSum(5, 7);
+		System.out.println(check);
+		printSign(-1);
+		printSign(1);
 	}	
 	
 	private static boolean checkSum(int k, int d) {
@@ -8,6 +12,14 @@ public class HomeWorkApp2 {
 			return true;
 		} else {
 			return false;
+		}
+	}
+	
+	private static void printSign(int num) {
+		if (num < 0) {
+			System.out.println("Negative");
+		} else {
+			System.out.println("Positive");
 		}
 	}
 }
