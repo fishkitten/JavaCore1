0437039584b9e323ff2e40d086d25dd4123bf10c
fishkitten <_bumble-bee@mail.ru>
2021-10-18 20:23:52 +0300

    HomeWork 3 task1

--- HomeWorkApp3.java
+++ HomeWorkApp3.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
+
 public class HomeWorkApp3 {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		task1();	
+	}
+	
+	public static void task1() {//task
+		int[] numbers = {1,0,0,1,1,1};
+		for(int i = 0; i < numbers.length; i++) {
+			if (numbers[i] == 0) {
+				numbers[i] = 1;
+			} else {
+				numbers[i] = 0;
+			}
+	    }
+		System.out.println(Arrays.toString(numbers));
 	}
 }
